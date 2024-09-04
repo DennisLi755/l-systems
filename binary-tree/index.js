4e30c6adfe6bd4c51262cf9c39cb9586dbb9e182
DennisLi755 <djl6347@g.rit.edu>
2024-09-04 10:34:03 -0400

    binary tree completed

--- binary-tree/index.js
+++ binary-tree/index.js
@@ -15,7 +15,7 @@ const axiom = "0";
 
 // lindenmayer string creation
 let mutableAxiom = axiom;
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 7; i++) {
     let string = '';
     for (let j = 0; j < mutableAxiom.length; j++) {
         const letter = mutableAxiom[j];
@@ -33,41 +33,39 @@ for (let i = 0; i < 5; i++) {
 let startingPoint = [0 ,0];
 let currentAngle = -90;
 let currentPosition = startingPoint;
-let lineDistance = 5;
-let points = [startingPoint];
+let lineDistance = 3;
+let points = [[startingPoint]];
 let stateStack = [];
 
 const interpretation = {
     '0': () => {
-        //currentPosition = points.slice(-1)[0];
+        currentPosition = points.slice(-1)[0].slice(-1)[0];
         const nextPoint = [
             currentPosition[0] + lineDistance * Math.cos(currentAngle * Math.PI / 180),
             currentPosition[1] + lineDistance * Math.sin(currentAngle * Math.PI / 180),
         ];
 
-        points.push(nextPoint);
-        currentPosition = nextPoint;
+        points.slice(-1)[0].push(nextPoint);
     },
     '1': () => {
-        //currentPosition = points.slice(-1)[0];
+        currentPosition = points.slice(-1)[0].slice(-1)[0];
         const nextPoint = [
             currentPosition[0] + lineDistance * Math.cos(currentAngle * Math.PI / 180),
             currentPosition[1] + lineDistance * Math.sin(currentAngle * Math.PI / 180),
         ];
 
-        points.push(nextPoint);
-        currentPosition = nextPoint;
+        points.slice(-1)[0].push(nextPoint);
     },
     '[': () => {
         stateStack.push({
-            position: points.slice(-1)[0],
+            position: points.slice(-1)[0].slice(-1)[0],
             angle: currentAngle,
         });
         currentAngle -= 45;
     },
     ']': () => {
         const lastState = stateStack.pop();
-        currentPosition = lastState.position;
+        points.push([lastState.position]);
         currentAngle = lastState.angle;
         currentAngle += 45;
     }
@@ -80,19 +78,26 @@ for (let i = 0; i < mutableAxiom.length; i++) {
     interp();
 }
 console.log(mutableAxiom);
+console.log(points);
 
 const p = points;
 
-const polyline = document.createElementNS(
-    'http://www.w3.org/2000/svg', 
-    'polyline'
-);
-polyline.setAttribute('points', points.join(' '));
-polyline.setAttribute('fill', 'none');
-polyline.setAttribute('stroke', 'black');
-polyline.setAttribute('stroke-width', '1px');
+let polyline = [];
+for (let i = 0; i < points.length; i++) {
+    polyline[i] = document.createElementNS(
+        'http://www.w3.org/2000/svg', 
+        'polyline'
+    );
+    polyline[i].setAttribute('points', points[i].join(' '));
+    polyline[i].setAttribute('fill', 'none');
+    polyline[i].setAttribute('stroke', 'black');
+    polyline[i].setAttribute('stroke-width', '1px');
+}
+
 
 // add the polyline to the svg
 const svg = document.querySelector('svg');
-svg.appendChild(polyline);
+for (let i = 0; i < polyline.length; i++) {
+    svg.appendChild(polyline[i]);
+}
   
